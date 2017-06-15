52cf9ccbda639fb45417758a838d88216f7bb773
John Faithorn <john.faithorn@futurenet.com>
2017-06-15 10:31:04 -0700

    remove cruft

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@
 import navigation from './js/navigation.js';
 
 
-$(document).ready(()=> {
-    navigation();
-    console.log("jquery here");
-
-    $(window).scroll(()=> {
-        let top = $(window).scrollTop();
-        let height = $(window).height();
-        let t_height = $(document).height();
-    //    console.log(top+(height/2));
-
-        $("#page").css({"perspective-origin":"50% " + (top+(height/2))+"px"})
-    })
-
-})
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  });
 
 
 
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,40 +1,7 @@
 //https://stackoverflow.com/questions/33611812/javascript-es6-export-const-vs-export-default
 
 export default function navigation() {
-	const button = $("#drawer-button");
-	const close = $("#drawer-close");
-	const main = $("#main");
-	const nav = $("#main-nav");
-	const page = $("#page");
-	const drawer = $("#mobile-nav");
-	const search_button = $("#search-button");
-	const search = $("#search");
-
-	button.on("click",function(e){
-		e.preventDefault();
-		e.stopPropagation();
-		console.log("clicked");
-		main.toggleClass("open");
-		nav.toggleClass("open");
-		page.toggleClass("open");
-		drawer.toggleClass("open");
-	})
-
-	close.on("click",function(e){
-		e.preventDefault();
-		e.stopPropagation();
-		main.removeClass("open");
-		nav.removeClass("open");
-		page.removeClass("open");
-		drawer.removeClass("open");
-	})
-
-	search_button.on("click",function(e){
-		console.log("clicked");
-		e.preventDefault();
-		e.stopPropagation();
-		search.toggleClass('open');
-	})
+	console.log("nav");
 
 }
 
